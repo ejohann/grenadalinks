105644f12a9e8cb42099d312987e0ef599e9443c
Johanne <johanne_lewis@hotmail.com>
2019-06-08 10:43:30 +0100

    Updated coming soon info

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
  <html>
    <head> 
-     <title>Hanne Digital</title>
+     <title>Grenada Links</title>
      <meta charset='utf-8'>
      <meta name='viewport' content='width=device-width, initial-scale = 1.0' />
      <link rel='stylesheet' href='./styles.css' />
@@ -14,15 +14,17 @@
        </div>
      </div>
      <footer> 
-       <p class='coming_soon'>Coming Soon!!!</p>
-       <p class='copyright'>&copy;Hanne Digital <?php echo date("Y"); ?>.  
+      
+
+       <p class='coming_soon'>Coming Soon!!!<br/>This website is being developed by DCM Grenada</p>
+       <p class='copyright'>&copy;Grenada Links<?php echo date("Y"); ?>.  
                             All rights reserved</p>
      </footer>
      <script>
 	   new Vue({
 			      el: '#exercise',
 			      data: {
-				           link: './hd_logo.png'
+				           link: './dcm_logo.png'
 			            }
 			  })
 	 </script>
